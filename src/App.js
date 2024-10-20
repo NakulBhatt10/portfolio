3d243929d43d330f7309e85404b2d95881f25e30
nakulbhatt10 <nakulbhatt610@gmail.com>
2024-10-20 23:46:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import Navbar from './components/Navbar'; // Importing the Navbar component
-import './App.css'; // Importing global styles (if needed)
-import LeftSide from './components/LeftSide';
+import Navbar from './components/Navbar';
+import './App.css';
 import About from './components/sections/About/About';
-import Contacts from './components/sections/Contacts/contacts';
+import Home from './components/sections/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <LeftSide />
+      <Home />
       <About />
-      <Contacts />
     </div>
   );
 }
